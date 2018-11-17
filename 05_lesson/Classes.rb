05643dd76b125ea75cb498f0fcc48972feb757b2
Ilya <ec2-user@ip-172-31-8-249.eu-west-1.compute.internal>
2018-11-17 11:08:45 +0000

    05_lesson

--- 05_lesson/Classes.rb
+++ 05_lesson/Classes.rb
@@ -1,17 +1,30 @@
 class Player
-    attr_reader :lastname
-    attr_writer :lastname
-    attr_accessor :age, :name
+    attr_accessor:name,:lastname,:age
 def info 
     puts "This class gives information about players. Their name,lastname and age "
 end 
 def greeting 
     puts "Hello I'm #{@name} #{@lastname}. I'm #{@age} yers old."
 end 
+def ready_to_start
+    puts "Player is ready to start the game."
 end 
+end 
+
 class Piece
-    
+    attr_accessor :name,:movement
+    def info 
+        puts "This class gives information about pices. Their colour,name and how do they moves."
+    end 
 end 
+
+class Game
+    attr_accessor :duration, :number_of_step
+    def info
+        puts "This class gives information about duration of the game and the number of step"
+    end 
+end 
+
 
 player1=Player.new
 player1.info
@@ -19,5 +32,10 @@ player1.name='Ilya'
 player1.lastname='Gorbunov'
 player1.age=15
 player1.greeting
+
+player2=Player.new
+player2.name='AI'
+player2.age=1002
+player2.greeting
 
 
